82f9800c82c79a1aee3088e5bbae9188d88359e3
yangtianyu <535173883@qq.com>
2021-12-06 22:47:24 +0800

    .

--- vuecli/ant-design-vue-pro/src/layout/SiderMenu.vue
+++ vuecli/ant-design-vue-pro/src/layout/SiderMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <div style="width: 256px">
     <a-menu
-      :default-selected-keys="['1']"
-      :default-open-keys="['2']"
+      :selected-keys="selectedKeys"
+      :open-keys="openKeys"
       mode="inline"
       :theme="navTheme"
       :inline-collapsed="collapsed"
@@ -32,26 +32,47 @@ export default {
   components: {
     "sub-menu": SubMenu,
   },
+  watch: {
+    "$route.path": function (val) {
+      this.selectedKeys = this.selectedKeysMap[val];
+      this.openKeys = this.collapsed ? [] : this.openKeysMap[val];
+    },
+  },
   data() {
+    this.selectedKeysMap = {};
+    this.openKeysMap = {};
     const menuData = this.getMenuData(this.$router.options.routes);
     return {
       collapsed: false,
       list: [],
       menuData,
+      selectedKeys: this.selectedKeysMap[this.$route.path],
+      openKeys: this.collapsed ? [] : this.openKeysMap[this.$route.path],
     };
   },
   methods: {
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
     },
-    getMenuData(routes) {
+    getMenuData(routes = [], parentKeys = [], selectedKey) {
       const menuData = [];
       routes.forEach((item) => {
         if (item.name && !item.hideInMenu) {
+          this.openKeysMap[item.path] = parentKeys;
+          this.selectedKeysMap[item.path] = [item.path || selectedKey];
           const newItem = { ...item };
           delete newItem.children;
           if (item.children && !item.hideChildrenInMenu) {
-            newItem.children = this.getMenuData(item.children);
+            newItem.children = this.getMenuData(item.children, [
+              ...parentKeys,
+              item.path,
+            ]);
+          } else {
+            this.getMenuData(
+              item.children,
+              selectedKey ? parentKeys : [...parentKeys, item.path],
+              selectedKey || item.path
+            );
           }
           menuData.push(newItem);
         } else if (
@@ -59,7 +80,10 @@ export default {
           !item.hideChildrenInMenu &&
           item.children
         ) {
-          menuData.push(...this.getMenuData(item.children));
+          menuData.push(...this.getMenuData(item.children), [
+            ...parentKeys,
+            item.path,
+          ]);
         }
       });
       console.log(menuData, "--------------");
