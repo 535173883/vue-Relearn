d1ee0aa96346ddf73aaff2d7ad4192887d353879
yangtianyu <535173883@qq.com>
2021-12-09 01:49:49 +0800

    表单

--- vuecli/ant-design-vue-pro/src/views/Forms/BasicForm.vue
+++ vuecli/ant-design-vue-pro/src/views/Forms/BasicForm.vue
@@ -1,9 +1,88 @@
 <template>
-  <div>BasicForm</div>
+  <a-form :layout="formLayout" :form="form">
+    <a-form-item
+      label="Form Layout"
+      :label-col="formItemLayout.labelCol"
+      :wrapper-col="formItemLayout.wrapperCol"
+    >
+      <a-radio-group
+        default-value="horizontal"
+        @change="handleFormLayoutChange"
+      >
+        <a-radio-button value="horizontal"> Horizontal </a-radio-button>
+        <a-radio-button value="vertical"> Vertical </a-radio-button>
+        <a-radio-button value="inline"> Inline </a-radio-button>
+      </a-radio-group>
+    </a-form-item>
+    <a-form-item
+      label="Field A"
+      :label-col="formItemLayout.labelCol"
+      :wrapper-col="formItemLayout.wrapperCol"
+    >
+      <a-input
+        placeholder="input placeholder"
+        v-decorator="[
+          'FieldA',
+          { rules: [{ required: true, min: 5, message: '必须大于五个字符' }] },
+        ]"
+      />
+    </a-form-item>
+    <a-form-item
+      label="Field B"
+      :label-col="formItemLayout.labelCol"
+      :wrapper-col="formItemLayout.wrapperCol"
+    >
+      <a-input placeholder="input placeholder" v-decorator="['FieldB']" />
+    </a-form-item>
+    <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
+      <a-button type="primary" @click="handleSubmit"> Submit </a-button>
+    </a-form-item>
+  </a-form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      form: this.$form.createForm(this),
+      formLayout: "horizontal",
+    };
+  },
+  computed: {
+    formItemLayout() {
+      const { formLayout } = this;
+      return formLayout === "horizontal"
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+          }
+        : {};
+    },
+    buttonItemLayout() {
+      const { formLayout } = this;
+      return formLayout === "horizontal"
+        ? {
+            wrapperCol: { span: 14, offset: 4 },
+          }
+        : {};
+    },
+  },
+  methods: {
+    handleSubmit() {
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+        }
+      });
+    },
+    handleFormLayoutChange(e) {
+      this.formLayout = e.target.value;
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.form.setFieldsValue({ FieldA: "hello world" });
+    }, 3000);
+  },
+};
 </script>
-
-<style></style>
